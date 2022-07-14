171fe7639ea713aa25f72a5473ad163b1463654f
Gabriela Ramires <engenheira.ramires@gmail.com>
2022-07-14 18:47:06 -0300

    Primeira Versão

---
+++ email.php
@@ -0,0 +1,36 @@
+<?php
+  //Variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $senha = $_POST['senha'];
+  $rep_senha = $_POST['rep_senha'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Telefone: </b>$telefone</p>
+      <p><b>Senha: </b>$senha</p>
+      <p><b>Confirme senha: </b>$rep_senha</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "engenheira.ramires@gmail.com";
+  $assunto = "Cadastro pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+?>
---
+++ index.js
@@ -0,0 +1,52 @@
+
+function VerificaSenha() {
+
+    var senha = document.querySelector('input[name=senha]');
+    var rep_senha = document.querySelector('input[name=rep_senha]');
+
+    var senhaValue = senha.value;
+    var rep_senhaValue = rep_senha.value;
+
+
+    var aviso = document.getElementById('aviso')
+    var fieldsetSenha = document.getElementById('Senha')
+    var fieldsetRep_senha = document.getElementById('Rep_senha')
+
+    if ((senhaValue == rep_senhaValue) && (senhaValue != "") && (rep_senhaValue != "")) {
+        rep_senha.setCustomValidity('');
+        aviso.setAttribute('style', 'display:none')
+        fieldsetSenha.setAttribute('style', 'border-color:green')
+        fieldsetRep_senha.setAttribute('style', 'border-color:green')
+
+
+    } else {
+        rep_senha.setCustomValidity('As senhas não conferem');
+        aviso.innerHTML = "As senhas não conferem"
+
+        aviso.setAttribute('style', 'display:visible')
+        fieldsetSenha.setAttribute('style', 'border-color:red')
+        fieldsetRep_senha.setAttribute('style', 'border-color:red')
+        fieldsetSenha.style.color = 'red'
+        fieldsetRep_senha.style.color = 'red'
+    }
+
+}
+
+
+var form = document.getElementById('form').onsubmit = function () {
+
+    return (
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Seu cadastro foi realizado com sucesso!',
+            showConfirmButton: false,
+            timer: 4000
+        })
+    )
+}
+
+
+
+
+
